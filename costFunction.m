c62c14cc4f9797f5fc3378bd95dd10919bacc615
김재훈 <kisow@navercorp.com>
2015-10-02 16:38:43 +0900

    불필요한 중간 결과값을 출력하지 않도록 수정

--- costFunction.m
+++ costFunction.m
@@ -20,8 +20,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-grad = (X' * (sigmoid(X * theta) - y)) / m
-J = (-1 / m) * (y' * log(sigmoid(X * theta)) + (1 - y') * log(1 - sigmoid(X * theta)))
+grad = (X' * (sigmoid(X * theta) - y)) / m;
+J = (-1 / m) * (y' * log(sigmoid(X * theta)) + (1 - y') * log(1 - sigmoid(X * theta)));
 
 
 % =============================================================
