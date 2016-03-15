37d2e183d8897e19fb9efe3eb0172c2d95a9134e
김재훈 <kisow.github.io@navercorp.com>
2016-03-15 20:56:21 +0900

    ex8: estimateGaussian.m 코드 정리

--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -20,7 +20,7 @@ sigma2 = zeros(n, 1);
 %               the data for the i-th feature and sigma2(i)
 %               should contain variance of the i-th feature.
 %'
-mu = ones(m, 1)' * X / m;
-sigma2 = ones(m, 1)' * (X - mu) .^ 2 / m;
+mu = sum(X) / m;
+sigma2 = sum((X - mu) .^ 2) / m;
 % =============================================================
 end
